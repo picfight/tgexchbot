31c5d90e7e433cdc03e097905957df1bcb126b88
J Fixby <github@jfixby.com>
2020-03-30 21:39:40 +0200

    swap conf

--- go.mod
+++ go.mod
@@ -1,11 +1,10 @@
 module github.com/picfight/tgexchbot
 
-go 1.12
+go 1.13
 
 require (
 	github.com/btcsuite/btcd v0.20.1-beta
-	github.com/btcsuite/btcutil v0.0.0-20190425235716-9e5f4b9a998d
-	github.com/jfixby/btcharness v0.0.0-20200327195549-3a52af262cc0
+	github.com/btcsuite/btcutil v1.0.1
 	github.com/jfixby/coinharness v0.0.0-20200327152748-5be5b892422b
 	github.com/jfixby/pin v0.0.0-20190926185208-4828e1e664f4
 	github.com/picfight/pfcd v0.0.0-20191229010435-dfe5cf45f91b
--- main.go
+++ main.go
@@ -12,22 +12,7 @@ func main() {
 	lang.CheckErr(err)
 	conf, err := cfg.ReadCfgFile(filePath)
 	lang.CheckErr(err)
-
-	{
-		pin.D("connect BTCD RPC")
-		btcconnect := cfg.RPCConnectionConfig{
-			Host:            conf.BTCDConfig.Host,
-			User:            conf.BTCDConfig.User,
-			Pass:            conf.BTCDConfig.Pass,
-			CertificateFile: conf.BTCDConfig.CertificateFile,
-		}
-
-		client, err := cfg.NewBTCConnection(btcconnect)
-		lang.CheckErr(err)
-		hash, err := client.GetBlockHash(0)
-		lang.CheckErr(err)
-		pin.D("hash", hash)
-	}
+	pin.S("conf", conf)
 
 	{
 		pin.D("connect PFCD RPC")
@@ -39,6 +24,21 @@ func main() {
 		}
 
 		client, err := cfg.NewPFCConnection(pfcconnect)
+		lang.CheckErr(err)
+		hash, err := client.GetBlockHash(0)
+		lang.CheckErr(err)
+		pin.D("hash", hash)
+	}
+	{
+		pin.D("connect BTCD RPC")
+		btcconnect := cfg.RPCConnectionConfig{
+			Host:            conf.BTCDConfig.Host,
+			User:            conf.BTCDConfig.User,
+			Pass:            conf.BTCDConfig.Pass,
+			CertificateFile: conf.BTCDConfig.CertificateFile,
+		}
+
+		client, err := cfg.NewBTCConnection(btcconnect)
 		lang.CheckErr(err)
 		hash, err := client.GetBlockHash(0)
 		lang.CheckErr(err)
