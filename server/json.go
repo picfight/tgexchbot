44428e4a73ec8bfe923c974a5a9831c58ed5f9ff
J Fixby <github@jfixby.com>
2020-04-09 22:42:48 +0200

    json

--- server/json.go
+++ server/json.go
@@ -7,12 +7,12 @@ type Rate struct {
 	BTCperPFC              float64 `json: "BTCperPFC"`
 }
 
-type AddressString struct {
-	AddressString string `json: "address_string"`
-	Type          string `json: "type"`
-}
-
 type StringAnalysis struct {
 	BTCAddress *AddressString `json: "btc_address"`
 	PFCAddress *AddressString `json: "pfc_address"`
+}
+
+type AddressString struct {
+	AddressString string `json: "address_string"`
+	Type          string `json: "type"`
 }
