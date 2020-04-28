855d6d561e11330aa97ba917cd5bf4e5fe2e1d35
J Fixby <github@jfixby.com>
2020-04-28 22:21:08 +0200

    FS

--- org.picfight.exchbot.lambda/src/org/picfight/exchbot/lambda/FilesystemSetup.java
+++ org.picfight.exchbot.lambda/src/org/picfight/exchbot/lambda/FilesystemSetup.java
@@ -11,6 +11,14 @@ public class FilesystemSetup {
 		final FilesystemSetup setup = this;
 		setup.Root = root;
 
+		setup.Orders = setup.Root.child("orders");
+		setup.Newo = setup.Orders.child("new");
+		setup.Executed = setup.Orders.child("executed");
+		setup.Expired = setup.Orders.child("expired");
+		setup.Processing = setup.Orders.child("processing");
+		setup.Error = setup.Processing.child("error");
+		setup.NoEnoughBTC = setup.Processing.child("no_btc");
+		setup.NoEnoughPFC = setup.Processing.child("no_pfc");
 	}
 
 	public File Root;
@@ -27,31 +35,13 @@ public class FilesystemSetup {
 
 	public void deploy () throws IOException {
 		final FilesystemSetup setup = this;
-		setup.Orders = setup.Root.child("orders");
-		{
-			setup.Newo = setup.Orders.child("new");
-			setup.Newo.makeFolder();
 
-			setup.Executed = setup.Orders.child("executed");
-			setup.Executed.makeFolder();
-
-			setup.Expired = setup.Orders.child("expired");
-			setup.Expired.makeFolder();
-
-			setup.Processing = setup.Orders.child("processing");
-// setup.Processing.makeFolder();
-
-			{
-				setup.Error = setup.Processing.child("error");
-				setup.Error.makeFolder();
-
-				setup.NoEnoughBTC = setup.Processing.child("no_btc");
-				setup.NoEnoughBTC.makeFolder();
-
-				setup.NoEnoughPFC = setup.Processing.child("no_pfc");
-				setup.NoEnoughPFC.makeFolder();
-			}
-		}
+		setup.Newo.makeFolder();
+		setup.Executed.makeFolder();
+		setup.Expired.makeFolder();
+		setup.Error.makeFolder();
+		setup.NoEnoughBTC.makeFolder();
+		setup.NoEnoughPFC.makeFolder();
 
 	}
 
