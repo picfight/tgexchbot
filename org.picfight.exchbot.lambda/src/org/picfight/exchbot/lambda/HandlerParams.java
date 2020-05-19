84bd117c3f3a6161719ae8410b5b24705fa318e5
J Fixby <github@jfixby.com>
2020-05-19 22:46:29 +0200

    stash

---
+++ org.picfight.exchbot.lambda/src/org/picfight/exchbot/lambda/HandlerParams.java
@@ -0,0 +1,13 @@
+
+package org.picfight.exchbot.lambda;
+
+import com.jfixby.scarabei.api.file.File;
+
+public class HandlerParams {
+
+	public File orderFile;
+	public FilesystemSetup fileSystem;
+	public Transaction transaction;
+	public Long deadline;
+
+}
