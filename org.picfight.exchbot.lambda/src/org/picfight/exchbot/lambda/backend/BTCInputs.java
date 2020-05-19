821dc0747e3b71d40de228811bca488e1aa3ec74
J Fixby <github@jfixby.com>
2020-05-19 22:46:21 +0200

    stash

---
+++ org.picfight.exchbot.lambda/src/org/picfight/exchbot/lambda/backend/BTCInputs.java
@@ -0,0 +1,10 @@
+
+package org.picfight.exchbot.lambda.backend;
+
+public class BTCInputs {
+
+	public int size () {
+		return 0;
+	}
+
+}
---
+++ org.picfight.exchbot.lambda/src/org/picfight/exchbot/lambda/backend/PFCInputs.java
@@ -0,0 +1,10 @@
+
+package org.picfight.exchbot.lambda.backend;
+
+public class PFCInputs {
+
+	public int size () {
+		return 0;
+	}
+
+}
